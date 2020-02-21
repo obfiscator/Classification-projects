45aee2359d97e689f9c5a4d34d348a058f9be0f2
Matt McGrath <obfiscator@gmail.com>
2020-02-21 10:03:49 +0100

    Writing frequency analysis to file to allow faster running; adding better consideration of plural words.

--- classify_pdfs.py
+++ classify_pdfs.py
@@ -17,9 +17,14 @@ import string
 from collections import Counter
 import nltk
 from nltk.corpus import words
+import json
+
+# Use a global debug variable to quickly shift between some test cases
+# and the amount of output I write.
+ldebug=True
 
 def extract_text_from_pdf(pdf_path):
-    
+
     # These lines seem to be something always needed for initialization
     resource_manager = PDFResourceManager()
     fake_file_handle = StringIO()
@@ -56,40 +61,77 @@ def extract_text_from_pdf(pdf_path):
     #endif
 #enddef
 
-def create_dictionary(wordlist,keyword_file,rejected_words_file):
-    # From a word list, ask the user if they want to add this
-    # word to a list of "good" words that I will use to fingerprint
-    # articles, or a list of "bad" words that will be ignored.
+def create_dictionary(wordlist):
+    # Trim down a word list, since the word list will include a lot
+    # of things that aren't really words.
     # Do a first triage with words found in the WordNet database.
-    # Not all words are there, but it's a good start.
+    # Not all words are there (including plurals), but it's a good start.
+    # This routine takes a little long.
 
     print("Working on creating dictionary.")
 
-    # Take a list of words from some classic books from nltk.
+    # This is very slow, likely because words.words() is large
+    # Is it really necessary?  I guess better to compare against
+    # a set of words.  I don't want to lose the frequency information,
+    # though.  So, first do that, and then put the frequency
+    # information in a dictionary to keep for later.
+    wordcounts=Counter(wordlist)
 
-    try:
-        f_keyword=open(keyword_file,"r+")
-    except:
-        f_keyword=open(keyword_file,"w")
-    #endtry
-
-    try:
-        f_reject=open(rejected_words_file,"r+")
-    except:
-        f_reject=open(rejected_words_file,"w")
+    # the Counter object has a nice feature that lets us grab the
+    # most popular words.  The problem is that it does output a 
+    # list instead of a dictionary.
+    if ldebug:
+        wordlist=Counter(wordlist).most_common(50)
+        wordcounts={}
+        for word,freq in wordlist:
+            wordcounts[word]=freq
+        #endfor
     #endif
 
-    # This is very slow, likely because words.words() is large
-    for word in wordlist:
-        if word.lower() in words.words():
-            print("Found word! ",word)
-        else:
-            print("Did not find word! ",word)
+    retained_words_freq={}
+
+    # I will keep any word that I find in the dictionary.
+    # Notice that I need to convert the word to lowercase, since
+    # it appears that all words in words are lowercase.
+    for word in wordcounts.keys():
+
+        # Try a couple different variations of the word,
+        # stripping off the trailing S and ES.  The way this
+        # loop is written, I cannot distinguish between "a" and
+        # "as", for example, but I hope that cases like that are
+        # few and far between.  I feel this is still much better
+        # than missing all plural words, or as counting plural and
+        # singular words as seperate enteries.
+        wordtry=[word.lower()]
+        if word.endswith(("s","S")):
+            wordtry.append(re.sub(r"s$",r"",word.lower()))
         #endif
+        if word.endswith(("es","ES")):
+            wordtry.append(re.sub(r"es$",r"",word.lower()))
+        #endif
+        if word.endswith(("ies","IES")):
+            wordtry.append(re.sub(r"ies$",r"y",word.lower()))
+        #endif
+
+        for word_variation in wordtry:
+            if ldebug:
+                print("Trying word: ",word_variation)
+            #endif
+            if word_variation in words.words():
+                if ldebug:
+                    print("Found word: ",word_variation,word)
+                #endif
+                retained_words_freq[word_variation]=wordcounts[word]
+            else:
+                if ldebug:
+                    print("Did not find word: ",word_variation)
+                #endif
+            #endif
+        #endfor
     #endfor
 
-    f_keyword.close()
-    f_reject.close()
+    return(retained_words_freq)
+
 #enddef
 
 def create_wordlist(text):
@@ -99,7 +141,10 @@ def create_wordlist(text):
 
     # This is far from perfect.  Units, for example, show up as words.
 
+    # Making all capital letters harmonizes things, but now acronymes
+    # that are words will be counted as words.  Nothing to be done, though.
     wordlist=text.upper()
+
     # join together any words seperated by a hyphen by removing the hypen and
     # any non-letter that comes after (such as a newline)
     wordlist=re.sub(r'-\W+',r'',wordlist)
@@ -107,38 +152,20 @@ def create_wordlist(text):
     wordlist=re.sub(r'\W',r' ',wordlist)
     wordlist=re.sub(r'\d',r' ',wordlist)
 
+    # Do I try to undo plurals here?  Removing s and es at the end
+    # of words?  Plurals aren't recognized in the NLTK word list
+    # that I check against later.  This won't catch a word like 
+    # "energies".  Perhaps it's better to do this leter?
+    #wordlist=re.sub(r'ES\s',r' ',wordlist)
+    #wordlist=re.sub(r'S\s',r' ',wordlist)
+
     # split the text into individual words based on whitespace
     wordlist=wordlist.split()
 
     return wordlist
 #enddef
 
-def process_wordlist(wordlist):
-    # Takes a list of words.  Find out what the ten most
-    # popular words are, excluding some basic words like "the" and "a".
-    # Return those words and their counts.
-
-    print("How many words in this text? {0}".format(len(wordlist)))
-    wordcounts=Counter(wordlist)
-    for word in wordcounts.keys():
-        print(word,wordcounts[word])
-
-    return
-        
-#enddef
-
-    
 if __name__ == '__main__':
-
-    # This file contains a list of words that we will use
-    # to classify according to, based on user input.  We will
-    # also include words in a standard dictionary.
-    keyword_file="keywords.txt"
-
-    # This file contains a list of words that we have asked
-    # the user if they are "good" and the user has responded
-    # that they are not.
-    rejected_words_file="rejected_words.txt"
 
     # This needs to be done the first time the script is run, if you
     # don't already have words
@@ -152,23 +179,44 @@ if __name__ == '__main__':
         filename = os.fsdecode(file)
         if filename.endswith(".pdf"): 
             print("Processing {}".format(filename))
-            if filename == "abramowitz2012.pdf":
+            # Check to see if our processed .txt file
+            # exists.  If so, we don't want to redo all the work.
+            txt_filename=re.sub(r".pdf$",r".txt",filename)
+            try:
+                f_txt=open(txt_filename,"r")
+                f_txt.close()
+                lfound=True
+            except:
+                lfound=False
+            #endif
+            print("Do we have txt file? ",txt_filename,lfound)
+            # Do we have a processed .txt file already?
+            if not lfound:
+                #if filename == "abramowitz2012.pdf":
+                if True:
                 
-                # Get the text in a string format.
-                text=extract_text_from_pdf(filename)
-                print(text)
+                    # Get the text in a string format.
+                    text=extract_text_from_pdf(filename)
+                    if ldebug:
+                        print(text)
+                    #endif
+                    
+                    # Create a list of words from this string.
+                    wordlist=create_wordlist(text)
+                    
+                    # Check to see if these words are good or not
+                    retained_words_freq=create_dictionary(wordlist)
+                    
+                    # Write this to a .txt file.
+                    with open(txt_filename, 'w') as file:
+                        file.write(json.dumps(retained_words_freq))
+                    #endwith
 
-                # Create a list of words from this string.
-                wordlist=create_wordlist(text)
-
-                # Check to see if these words are good or not
-                create_dictionary(wordlist,keyword_file,rejected_words_file)
-               
-                # Get some statistics on these words
-                word_counts=process_wordlist(wordlist)
-
+                #endif
             #endif
         #endif
+    #endfor
+#endif
 
 
 
